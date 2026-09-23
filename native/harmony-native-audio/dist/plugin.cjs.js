5c21f4db11090dec7c1a8bbf118bead58e6eef4a
Lorenzomeravillia <145249429+Lorenzomeravillia@users.noreply.github.com>
2026-09-23 17:38:08 +0200

    Add CommonJS entry for native audio plugin

---
+++ native/harmony-native-audio/dist/plugin.cjs.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const core = require('@capacitor/core');
+
+const HarmonyNativeAudio = core.registerPlugin('HarmonyNativeAudio');
+
+exports.HarmonyNativeAudio = HarmonyNativeAudio;
